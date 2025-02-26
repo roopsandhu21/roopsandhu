eec0080c444474fb23b2001396b677a0e2df5221
unknown <bhupinderkanwar21@gmail.com>
2025-02-26 09:52:49 +0530

    changes are done

--- softech/express.js
+++ softech/express.js
@@ -1,8 +1,7 @@
 const express = require('express')
 const indexRouter=require('./Router/index')
 const app = express();
-app.use(express.json())
-// hello world how r you
+//app.use(express.json())
 app.use('/',indexRouter)
 app.listen(3000, () => {
     console.log("server started on port 3000")
